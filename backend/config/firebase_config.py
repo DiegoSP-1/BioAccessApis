21b9be0e7cd9f8d0fcf534b916612dcfd54ed34f
DiegoSP-1 <diegosp.ti24@utsjr.edu.mx>
2026-04-13 23:04:10 -0600

    Update Firebase config to load credentials from env
    Refactor Firebase initialization to use environment variable for credentials.

--- backend/config/firebase_config.py
+++ backend/config/firebase_config.py
@@ -1,8 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
+import json
+from firebase_admin import credentials, initialize_app
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_json = os.environ.get("FIREBASE_CREDENTIALS")
+
+cred_dict = json.loads(firebase_json)
+cred = credentials.Certificate(cred_dict)
+
+initialize_app(cred)
 
 firebase_admin.initialize_app(cred)
 
