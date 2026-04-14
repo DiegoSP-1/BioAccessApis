c39045f4842dcbfcb0101e29e10819e1d31a70cd
DiegoSP-1 <diegosp.ti24@utsjr.edu.mx>
2026-04-13 23:09:07 -0600

    Refactor Firebase initialization and check for existing app

--- backend/config/firebase_config.py
+++ backend/config/firebase_config.py
@@ -1,17 +1,13 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 import os
 import json
-from firebase_admin import credentials, initialize_app
+import firebase_admin
+from firebase_admin import credentials
 
-firebase_json = os.environ.get("FIREBASE_CREDENTIALS")
+firebase_json = os.getenv("FIREBASE_CREDENTIALS")
 
-cred_dict = json.loads(firebase_json)
-cred = credentials.Certificate(cred_dict)
+cred = credentials.Certificate(json.loads(firebase_json))
 
-initialize_app(cred)
-
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
