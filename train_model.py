6949418b7c141b3155b383ab20b599c75e7b7e8d
DiegoSP-1 <diegosp.ti24@utsjr.edu.mx>
2026-04-16 00:06:51 -0600

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -4,6 +4,9 @@ import numpy as np
 import json
 from backend.config.firebase_config import db
 from backend.services.user_service import registrar_usuario
+from backend.config.firebase_config import bucket
+import requests
+import tempfile
 
 dataset_path = "dataset"
 
@@ -12,6 +15,46 @@ labels = []
 label_dict = {}
 
 current_label = 0
+
+blobs = bucket.list_blobs(prefix="dataset/")
+
+usuarios = {}
+
+for blob in blobs:
+    partes = blob.name.split("/")
+    
+    if len(partes) < 3:
+        continue
+    
+    nombre = partes[1]
+
+    if nombre not in usuarios:
+        usuarios[nombre] = []
+    
+    usuarios[nombre].append(blob.public_url)
+
+for nombre, imagenes in usuarios.items():
+
+    label_dict[current_label] = nombre
+
+    for url in imagenes:
+        try:
+            response = requests.get(url)
+            img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
+            image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces_detected = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            for (x, y, w, h) in faces_detected:
+                face = gray[y:y+h, x:x+w]
+                faces.append(face)
+                labels.append(current_label)
+
+        except:
+            pass
+
+    current_label += 1
 
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
