086753e9c4c5bd61f0aa208e9fa3803cf53fa0c9
DiegoSP-1 <diegosp.ti24@utsjr.edu.mx>
2026-04-15 19:30:55 -0600

    Add Firebase Storage initialization to config

--- backend/config/firebase_config.py
+++ backend/config/firebase_config.py
@@ -1,7 +1,7 @@
 import os
 import json
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, storage
 
 firebase_json = os.getenv("FIREBASE_CREDENTIALS")
 
@@ -13,4 +13,11 @@ cred_dict = json.loads(firebase_json)
 if not firebase_admin._apps:
     firebase_admin.initialize_app(credentials.Certificate(cred_dict))
 
+cred = credentials.Certificate("firebase.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'bioaccess-1074c.appspot.com'
+})
+
+
 db = firestore.client()
+bucket = storage.bucket()
